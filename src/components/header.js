0f2473044c9ef2b686a1c82f846b87fb3d2e9eda
Leslie Behum <llaurenb@gmail.com>
2020-03-19 19:58:56 -0400

    add sun, hamburger, water

--- src/components/header.js
+++ src/components/header.js
@@ -4,21 +4,42 @@ import {Navbar, Nav} from "react-bootstrap/"
 import 'typeface-roboto';
 import { Link } from "gatsby"
 import  "./header.css"
+import { HamburgerElastic } from 'react-animated-burgers'
 
-export default () => (
-<div>
+class Header extends React.Component {
+  state = {
+    isActive: false
+  }
 
-  <Navbar id="nav-color" expand="lg" variant="">
-    <Link className="navbar-brand" to="/">Leslie Behum</Link>
-    <Navbar.Toggle id="menu" className="ml-auto" aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Link className="nav-link" to="/about/">Who I Am</Link>
-            <Link className="nav-link" to="/portfolio/">What I've Made</Link>
-            <Link className="nav-link" to="/contact/">Say Hello</Link>
-          </Nav>
-      </Navbar.Collapse>
-  </Navbar>
+toggleButton = () => {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
 
-</div>
-)
+  render() {
+    return (
+      <div>
+
+        <Navbar id="nav-color" expand="lg" variant="">
+          <Link className="navbar-brand" to="/">Leslie Behum</Link>
+            <Navbar.Toggle>
+              <HamburgerElastic
+                 buttonWidth={30} 
+                 isActive={this.state.isActive} toggleButton={this.toggleButton} buttonColor="" barColor="black" />
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Link className="nav-link" to="/about/">Who I Am</Link>
+                  <Link className="nav-link" to="/portfolio/">What I've Made</Link>
+                  <Link className="nav-link" to="/contact/">Say Hello</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
+      </div>
+    )
+  }
+}
+
+export default Header
