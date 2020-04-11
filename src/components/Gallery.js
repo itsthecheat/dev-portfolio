c989e61e32f645f24736749c75d07188042f856d
Leslie Behum <llaurenb@gmail.com>
2020-04-11 14:23:44 -0400

    working in masonry

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,32 +3,38 @@ import React from "react"
 import Masonry from "react-masonry-component"
 import {useStaticQuery, graphql} from "gatsby"
 import Img from "gatsby-image"
+import styles from "./Gallery.module.css"
 
 const Gallery = () => {
   const data = useStaticQuery(graphql`
     {
-     allGalleryYaml {
-       edges {
-         node {
-           img {
-             childImageSharp {
-               fluid {
-                 ...GatsbyImageSharpFluid
-               }
-             }
-           }
-         }
-       }
-     }
-   }
+      allProjectsYaml {
+        edges {
+          node {
+            id
+            thumbnail {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   `)
-  console.log("================")
-  console.log(data.allGalleryYaml.edges)
-  console.log("================")
+
   return (
-    <div>{data.allGalleryYaml.edges.map(node =>
-        <p><Img fluid = {node.childImageSharp.fluid} /></p>
-    )}</div>
+
+<Masonry className={styles.galleryWrapper}>
+    {data.allProjectsYaml.edges.map( element => (
+      <div key={element.node.id} >
+        <Img         fluid={element.node.thumbnail.childImageSharp.fluid} />
+      </div>
+      ))}
+
+</Masonry>
   )
 }
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import Masonry from "react-masonry-component"
 import {Container, Row, Col} from "react-bootstrap"
 import Gallery from "../components/Gallery"
 
 export default () => (
-
-<Container>
+  <Container>
     <Row>
       <Col lg={8}>
         <Gallery />
       </Col>
     </Row>
-</Container>
+  </Container>
+
+
 )
