132ed5592cb7c9d3ca42990e00706ef9e3ce5229
Leslie Behum <llaurenb@gmail.com>
2020-06-18 18:05:49 -0400

    add animations layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,11 @@ import "typeface-dm-serif-display"
 
 export default ({ children }) => (
   <Container fluid>
-    <div className={layoutStyles.sun}></div>
+    <div className={`${layoutStyles.sun} ${layoutStyles.bounceInTop}`}></div>
     <Header />
       <div className={layoutStyles.container}>{children}
         <div className={layoutStyles.water}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 150" preserveAspectRatio="none"><path fill= "rgba(135, 206, 235, .7)" d="M385.44,-25.37 C474.60,69.37 395.03,16.08 338.03,211.47 L500.00,150.00 L500.00,0.00 Z" />
+          <svg className={layoutStyles.slideLeft} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 150" preserveAspectRatio="none"><path fill= "rgba(135, 206, 235, .7)" d="M385.44,-25.37 C474.60,69.37 395.03,16.08 338.03,211.47 L500.00,150.00 L500.00,0.00 Z" />
           </svg>
         </div>
         </div>
